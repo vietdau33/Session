8a1fc9c0d51ae4ca9c136632a3e2d6e2c26bc91d
vietdau33 <55265675+vietdau33@users.noreply.github.com>
2020-04-13 15:33:03 +0700

    Add files via upload

---
+++ session.php
@@ -0,0 +1,80 @@
+<?php 
+
+class Session{
+
+	protected $name;
+
+	public function __construct($name, $flagNew = false){
+		if(!isset($_SESSION)){
+			session_start();
+		}
+		if(!isset($_SESSION[$name])){
+			$_SESSION[$name] = [];
+		}
+		if(isset($_SESSION[$name]) && $flagNew){
+			unset($_SESSION[$name]);
+		}
+		$this->name = $name;
+	}
+	public function getName(){
+		return $this->name;
+	}
+	public function hasSession($name){
+		return !!$this->getSession($name);
+	}
+	public function get($name = null){
+		if(is_null($name))
+			return $_SESSION[$this->name];
+		return $this->getSession($name);
+	}
+	public function set($name, $value){
+		$name = explode('.', $name);
+		$this->setSection($_SESSION[$this->name], $name, $value);
+	}
+	public function unset($name){
+		return $this->unsetSession($_SESSION[$this->name], $name);
+	}
+	public function die(){
+		unset($_SESSION[$this->name]);
+		return true;
+	}
+	private function unsetSession(&$valire, $name){
+		$name = explode('.', $name);
+		$n = $name[count($name) - 1];
+		foreach ($name as $v) {
+			if(!isset($valire[$v])){
+				return true;
+			}
+			if($v == $n){
+				unset($valire[$v]);
+				return true;
+			}
+			array_splice($name, 0, 1);
+			$this->setSection($valire[$v], $name);
+			break;
+		}
+	}
+	private function setSection(&$valire, $name, $value){
+		$n = $name[count($name) - 1];
+		foreach ($name as $v) {
+			if($v == $n){
+				$valire[$v] = $value;
+				return true;
+			}
+			array_splice($name, 0, 1);
+			$this->setSection($valire[$v], $name, $value);
+			break;
+		}
+	}
+	private function getSession($name){
+		$name = explode('.', $name);
+		$valire = $_SESSION[$this->name];
+		foreach ($name as $n) {
+			if(!isset($valire[$n])){
+				return false;
+			}
+			$valire = $valire[$n];
+		}
+		return $valire;
+	}
+}
